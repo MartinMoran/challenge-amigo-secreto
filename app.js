3f39c730358153f8cc470788d31b876a77812fb9
Martin Federico Moran <martinfmoran81@gmail.com>
2025-08-12 19:22:45 -0300

    funcion reiniciarJuego y comentarios

--- app.js
+++ app.js
@@ -3,13 +3,14 @@
 
 let arrayAmigos = [];
 
+// Funcion para limpiar el input
 function limpiarInput(){
     document.getElementById("amigo").value = "";
 }
 
 function agregarAmigo(){
     let nombre = document.getElementById("amigo").value.trim();
-
+    // Condional para validar el nombre sin texto
     if (nombre !== ""){
         arrayAmigos.push(nombre);
         actualizarArray(nombre, "listaAmigos");
@@ -24,20 +25,39 @@ function sortearAmigo(){
         alert("No hay ningun amigo para sortear");
         return;
     }
-
+    // Generamos un valor para sortear el nombre
     let indiceRandom = Math.floor(Math.random() * arrayAmigos.length);
     let amigoSorteado = arrayAmigos[indiceRandom];
     actualizarArray(amigoSorteado, "resultado");
     
+    // Preguntamos si quiere reiniciar el juego
+
+    setTimeout( ()=> {
+        let respuesta = confirm("Deseas reiniciar el juego?");
+        if (respuesta){
+            reiniciarJuego();
+    }
+    }, 500);
+
 
 }
 
+// Funcion para reutilizar y generar un elemento li
 function actualizarArray(nombre, idElemento){
     let nuevoElemento = document.createElement("li");
     nuevoElemento.textContent = nombre;
     document.getElementById(idElemento).appendChild(nuevoElemento);
 }
 
+// Funcion que resetea todo
+function reiniciarJuego(){
+    arrayAmigos = [];
+
+    document.getElementById("listaAmigos").innerHTML = "";
+    document.getElementById("resultado").innerHTML = "";
+
+    limpiarInput();
+}
 
 
 
