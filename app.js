8eb8a8d5b2e1dd36e993e61a5bb96028c7f7a84f
Martin Federico Moran <martinfmoran81@gmail.com>
2025-08-12 18:11:27 -0300

    funcion agregarAmigo con validacion

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. 
+// Aquí deberás desarrollar la lógica para resolver el problema.
+
+let arrayAmigos = [];
+let nombre = "";
+
+function limpiarInput(){
+    document.getElementById("amigo").value = "";
+}
+
+function agregarAmigo(){
+    nombre = document.getElementById("amigo").value;
+    console.log(nombre);
+
+    if (nombre != ""){
+        arrayAmigos.push(nombre);
+        actualizarArray(nombre);
+        limpiarInput();
+    }else{
+        alert("DEBES INGRESAR UN NOMBRE VALIDO!!!");
+    }
+
+    console.log(arrayAmigos.length);
+}
+
+function sortearAmigo(){
+
+}
+
+function actualizarArray(nombre){
+    let nuevoElemento = document.createElement("li");
+    nuevoElemento.textContent = nombre;
+    document.getElementById("listaAmigos").appendChild(nuevoElemento);
+}
+
+
+
+
